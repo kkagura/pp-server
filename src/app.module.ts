103f97718d5b5f86a287aabb54c17c7a46081831
kkagura <wwwungj@163.com>
2026-02-05 09:27:09 +0800

    feat: 移除file相关逻辑

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,6 @@ import * as path from 'path';
 import { User } from './user/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { RedisModule } from '@nestjs-modules/ioredis';
-import { FileModule } from './file/file.module';
-import { File } from './file/file.entity';
 
 const envPath = path.resolve('.env');
 
@@ -35,7 +33,7 @@ const envPath = path.resolve('.env');
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'mysql', // 数据库类型
-        entities: [User, File], // 数据表实体
+        entities: [User], // 数据表实体
         host: configService.get('DB_HOST'), // 主机，默认为localhost
         port: configService.get<number>('DB_PORT'), // 端口号
         username: configService.get('DB_USER'), // 用户名
@@ -47,7 +45,6 @@ const envPath = path.resolve('.env');
     }),
     UserModule,
     AuthModule,
-    FileModule,
     RedisModule,
   ],
   controllers: [AppController],
