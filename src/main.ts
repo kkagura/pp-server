396352620dbfbe8246d659cea76ae65c1be1f440
kkagura <wwwungj@163.com>
2026-02-25 09:11:50 +0800

    feat: 添加路由前缀

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from './validation/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('pp')
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
